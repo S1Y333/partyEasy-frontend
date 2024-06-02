10786e31e733dc77f26857e2d3ab86933ed910f4
siyi zhang <siyizhang@siyis-MBP.ht.home>
2024-06-01 23:46:16 -0400

    add back firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+import { getAuth } from "firebase/auth";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "party-easy.firebaseapp.com",
+  projectId: "party-easy",
+  storageBucket: "party-easy.appspot.com",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// export
+export const auth = getAuth(app);
